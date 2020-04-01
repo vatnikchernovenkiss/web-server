f89682e3f9a6d2deafde2634125a447ac5a4cf2a
vatnikchernovenkiss <56398346+vatnikchernovenkiss@users.noreply.github.com>
2020-04-01 14:07:01 +0300

    fix

--- webserver/src/Entities/Cars.java
+++ webserver/src/Entities/Cars.java
@@ -4,16 +4,18 @@ import java.util.Set;
 import java.util.Date;
 
 public class Cars {
+	public enum upholstery {Polyester,Faux_Leather, Leather, Olefin, Cotton}
+	public enum colour {Green, Red, Blue, White, Black}
 	private int id;
 	private Models model_id;
 	private String registration_number;
 	private int number_of_seats;
-	private String upholstery;
+	private upholstery upholstery_type;
 	private double price;
 	private Date last_maintance_date;
 	private Set<Clients> test_drive_clients;
 	private double milage;
-	private String car_colour;
+	private colour car_colour;
 	private double engine_power;
 	private int number_of_doors;
 	private Developers developer_id;
@@ -26,12 +28,12 @@ public class Cars {
 	public int getId() {return id;}
 	public String getRegistration_number() {return registration_number;}
 	public int getNumber_of_seats() {return number_of_seats;}
-	public String getUpholstery() {return upholstery;}
+	public String getUpholstery() {return upholstery_type.toString();}
 	public double getPrice() {return price;}
 	public Date getLast_maintance_date() {return last_maintance_date;}
 	public Set<Clients> getTest_drive_clients() {return test_drive_clients;}
 	public double getMilage() {return milage;}
-	public String getCar_colour() {return car_colour;}
+	public String getCar_colour() {return car_colour.toString();}
 	public double getEngine_power() {return engine_power;}
 	public int getNumber_of_doors() {return number_of_doors;}
 	public Developers getDeveloper_id() {return developer_id;}
@@ -40,12 +42,12 @@ public class Cars {
 	public void setModel_id(Models model_id) {this.model_id = model_id;}
 	public void setRegistration_number(String registration_number) {this.registration_number = registration_number;}
 	public void setNumber_of_seats(int number_of_seats) {this.number_of_seats = number_of_seats;}
-	public void setUpholstery(String upholstery) {this.upholstery = upholstery;}
+	public void setUpholstery(String upholsteryy) {this.upholstery_type = upholstery.Leather;}
 	public void setPrice(double price) {this.price = price;}
 	public void setLast_maintance_date(Date last_maintenance_date) {this.last_maintance_date = last_maintenance_date;}
 	public void setTest_drive_clients(Set<Clients> test_drive_clients) {this.test_drive_clients = test_drive_clients;}
 	public void setMilage(double milage) {this.milage = milage;}
-	public void setCar_colour(String car_colour) {this.car_colour = car_colour;}
+	public void setCar_colour(String car_colour) {this.car_colour = colour.Green;}
 	public void setEngine_power(double engine_power) {this.engine_power = engine_power;}
 	public void setNumber_of_doors(int number_of_doors) {this.number_of_doors = number_of_doors;}
 	public void setDeveloper_id(Developers developer_id) {this.developer_id = developer_id;}
--- webserver/src/Entities/Orders.java
+++ webserver/src/Entities/Orders.java
@@ -2,14 +2,15 @@ package Entities;
 
 import java.util.Date;
 public class Orders {
+	public enum status {processing, test_drive, done, ready, supply}
 	private int id;
 	private Date date_and_time;
 	private Cars car_id;
 	private boolean if_test_drive_required;
 	private Clients client_id;
-	private String current_status;
-	public String getCurrent_status() {return current_status;}
-	public void setCurrent_status(String current_status) {this.current_status = current_status;}
+	private status current_status;
+	public String getCurrent_status() {return current_status.toString();}
+	public void setCurrent_status(String current_status) {this.current_status = status.valueOf(current_status);}
 	public int getId() {return id;}
 	public void setId(int id) {this.id = id;}
 	public Date getDate_and_time() {return date_and_time;}
